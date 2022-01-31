cc186344a516aa5ef4734a9c431426466b8cfbee
Avery Burke <averyburke@averys-mbp.lan>
2022-01-30 16:16:07 -0800

    the program in app 1 correctly uses the algorythm to calculate the centroid of a triangle. next step. refactor to use voroni

--- src/app.ts
+++ src/app.ts
@@ -1,230 +1,215 @@
-// import * as webglUtils from "webgl-utils.js";
-// const canvas = document.querySelector('canvas');
-// const gl = canvas.getContext('webgl2');
+import * as twgl from "twgl.js";
+const vs1 = `#version 300 es
 
-// //data
-// function createCone(edges){
-//     const vertices = new Array();
-//     vertices[0] = 0;
-//     vertices[1] = 0;
-//     vertices[2] = -1;
-//     for (let i = 0; i <= edges; i++) {
-//         const angle = 2 * Math.PI * i / edges;
-//         vertices[i*3 + 3] = Math.cos(angle);
-//         vertices[i*3 + 4] = Math.sin(angle);
-//         vertices[i*3 + 5] = 1;
-//     }
-//     return vertices;
-// }
+in vec2 a_position;
 
-// if (!gl) {
-//     throw new Error('WebGL not supported');
-// }
+void main() {
+    // gl_Position = vec4(a_position.xy - 1.0f, 1, 1);
+    gl_Position = vec4(a_position, 1, 1);
+}
+`
 
-// function rand(min, max) {
-//     if (max === undefined) {
-//       max = min;
-//       min = 0;
-//     }
-//     return min + Math.random() * (max - min);
-//   }
+const fs1 = `#version 300 es
 
-// const positionData = [...new Array(2000)].map(d => rand(-1,1))
-// const vertextData = createCone(36)
+precision highp float;
 
-// let conversionMat = [
-//     .5, 0, 0,//<--sx  
-//     0, .5, 0,//<--sy I think this is scaling for pixle density?
-//     .5, .5, 1//<--tx, ty translate 0,0 from texture space (<0,0> to <1,1>) to clip space
-// ]
+out ivec4 outColor;
 
-// //shaders
+void main() {
+    outColor.r = 0;
+}
+`
+
+const vs2 = `#version 300 es
+
+in vec2 a_texCoord;
+
+void main() {
+    // gl_Position = vec4(a_texCoord * 2.0f - 1.0f, 1, 1);
+    gl_Position = vec4(a_texCoord, 1, 1);
+}`
+
+const fs2 = `#version 300 es
+precision highp float;
+
+uniform mediump isampler2D tex;
+
+out vec4 color;
+
+void main() {
+    //to do: does gl_FragCoord need to be adusted for screen size or canvas size?
+    int my_index = int(gl_FragCoord.x);
+    for (int x = 0; x < 10; x++) {
+        ivec4 t = texelFetch(tex, ivec2(x, gl_FragCoord.y), 0);
+        if (t.r == my_index) {
+            color.r += float(x);
+            color.g += gl_FragCoord.y - 0.5f;
+            color.b += 1.0;
+        }
+    }
+}
+`
+
+const feedback_source = `#version 300 es
+
+out vec2 v_position;
+
+uniform sampler2D summed;
+
+void main() {
+    float count = 0.0;
+    for (int y = 0; y < 10; y++){
+        vec3 t = texelFetch(summed, ivec2(gl_VertexID, y), 0).xyz; 
+        v_position += t.xy;
+        count += t.z;
+    }
+    v_position /= count;
+}`
+
+const feedback_fragment_shader = `#version 300 es
+precision highp float;
+
+void main() {
+  discard;
+}`
+
+function createEmptyTexture(gl, targetTextureWidth, targetTextureHeight, mipLevel) {
+    let texture = gl.createTexture()
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texImage2D(gl.TEXTURE_2D, mipLevel, gl.RGB,
+        targetTextureWidth, targetTextureHeight, 0,
+        gl.RGB, gl.UNSIGNED_BYTE, null)
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+    return texture
+}
+
+const quad = new Float32Array([-
+    1,-1, 1,-1, -1,1,
+    1,-1, 1,1, -1,1
+]);
+
+const bufferArrays1 = {
+    a_position:{
+        numComponents:2,
+        data: new Float32Array([-1, 1, -.5 , -.5, 0, .5])
+        // data: quad,
+    }
+}
+
+const bufferArrays2 = {
+    a_texCoord:{
+        numComponents:2,
+        data: quad,
+    }
+}
+
+const feedbackArray = {
+    v_position:{
+        numComponents:2,
+        data: new Float32Array(16)
+    }
+}
+
+const canvas = document.querySelector('canvas')
+const gl = canvas.getContext('webgl2')
+if (!gl.getExtension("EXT_color_buffer_float")){
+    alert('no extention!!!')
+}
+const progarmInfo1 = twgl.createProgramInfo(gl, [vs1,fs1])
+const progarmInfo2 = twgl.createProgramInfo(gl,[vs2,fs2])
+const programInfo3 = twgl.createProgramInfo(gl, [feedback_source,feedback_fragment_shader], {transformFeedbackVaryings:["v_position"]})
+const bufferInfo1 = twgl.createBufferInfoFromArrays(gl,bufferArrays1)
+const bufferInfo2 = twgl.createBufferInfoFromArrays(gl,bufferArrays2)
+const feedbackBuferInfo = twgl.createBufferInfoFromArrays(gl,feedbackArray)
+
+const fbi = twgl.createFramebufferInfo(gl,[
+    //store only integer and only on the values on the red channel
+    {internalFormat: gl.R32I, format: gl.RED_INTEGER, type:gl.INT, minMag: gl.NEAREST,},
+  ], 10, 10);
+
+const sumBffer = twgl.createFramebufferInfo(gl,[
+    //store floating point vec4 values
+    {
+        internalFormat:gl.RGBA32F,
+        format:gl.RGBA,
+        type:gl.FLOAT,
+        minMag:gl.NEAREST}],1 ,10)
+        
+// const intermediateTexture = createEmptyTexture(gl, gl.canvas.width, gl.canvas.height, 0)
+// const targetTexture = createEmptyTexture(gl,1,canvas.height,0)
+
+const tf = gl.createTransformFeedback()
+gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, tf)
+gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, feedbackBuferInfo.attribs.v_position.buffer)
+gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, null)
+gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+function main() {
+
+    //program 1, write to intermediateTexture
+    gl.useProgram(progarmInfo1.program)
+    twgl.setBuffersAndAttributes(gl,progarmInfo1,bufferInfo1)
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbi.framebuffer)
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fbi.attachments[0], 0)
+    twgl.resizeCanvasToDisplaySize(gl.canvas)
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+    //set the background 'color' to -1//
+    gl.clearBufferiv(gl.COLOR, 0, new Int32Array([-1,0,0,0]))
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+
     
-// let vs1 = `#version 300 es
-// precision highp float;
-// in vec3 vertex; // position per vertex
-// in vec2 position; // offset per instance
-// out vec3 color;
-// void main() {
-//     color.r = float(gl_InstanceID % 256) / 255.0;
-//     color.g = float((gl_InstanceID / 256) % 256) / 255.0;
-//     color.b = float((gl_InstanceID / 65536) % 256) / 255.0;
-//     gl_Position = vec4(vertex.xy + position.xy, vertex.z, 1);
-// }
-// `
-
-// let fs1 = `#version 300 es
-// precision highp float;
-// in vec3 color;
-// out vec4 outColor;
-// void main() {
-//     outColor = vec4(color, 1.0);
-// }
-// `
-
-// const vs2 = `#version 300 es
-
-// in vec2 a_texCoord;
-// out vec2 v_texCoord;
-// uniform mat3 u_matrix;
-// void main() {
-//     // v_texCoord = vec3(u_matrix * vec3(a_texCoord,1)).xy;
-//     // v_texCoord = a_texCoord;
-//     gl_Position = vec4(a_texCoord,1.0,1.0);
-// }`
-
-// const fs2 = `#version 300 es
-// precision highp float;
- 
-// uniform sampler2D u_tex;
-// in vec2 v_texCoord;
- 
-// out vec4 outColor;
- 
-// void main() {
-//    outColor = texture(u_tex, v_texCoord);
-// outColor = vec4(0.0,0.8,0.0,1.0);
-// }`
-
-// function main () {
-//   console.time('time')
-// const vertexBuffer = gl.createBuffer();
-// gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertextData), gl.STATIC_DRAW);
-
-// const positionBuffer = gl.createBuffer();
-// gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positionData), gl.STATIC_DRAW); 
-
-// const vertexShader1 = createShader(gl,gl.VERTEX_SHADER,vs1)
-// const fragShader1 = createShader(gl,gl.FRAGMENT_SHADER,fs1)
-// const vertexShader2 = createShader(gl, gl.VERTEX_SHADER, vs2)
-// const fragmentShader2 = createShader(gl, gl.FRAGMENT_SHADER, fs2)
-
-// const prog = createProgram(gl, vertexShader1,fragShader1)
-// // const prog2 = createProgram(gl, vertexShader2, fragmentShader2)
-
-// ////////prog 1, write to texture///////////////////////////////
-
-// const vertexLocation = gl.getAttribLocation(prog, `vertex`);
-// gl.enableVertexAttribArray(vertexLocation);
-// gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-// gl.vertexAttribPointer(vertexLocation, 3, gl.FLOAT, false, 0, 0);
-
-// const positionLocation = gl.getAttribLocation(prog, `position`);
-// gl.enableVertexAttribArray(positionLocation);
-// gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-// gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-// gl.vertexAttribDivisor(positionLocation, 1);
-
-// gl.enable(gl.DEPTH_TEST);
-// gl.depthFunc(gl.LEQUAL);
-
-// gl.useProgram(prog);
-
-// // const mipLevel = 0
-// // var targetTexture = createEmptyTexture(gl,600,600,0)
-
-// //   const fb = gl.createFramebuffer();
-// //   gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-
-// // const attachmentPoint = gl.COLOR_ATTACHMENT0;
-// // gl.framebufferTexture2D(
-// // gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, targetTexture, mipLevel);
-// // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-// gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, vertextData.length/3, positionData.length/2);
-
-// ////////////////////////////////////////////////////
-// // //////////////prog2, read from textrue, write to screen///////////////////////////////////
-// // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
-// // let texCoordAttributeLocation = gl.getAttribLocation(prog2, "a_texCoord")
-// // var texCoordBuffer = gl.createBuffer();
-
-// // gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-// // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-// //     // Upper-left triangle
-// //     -1, -1,
-// //     1, -1,
-// //     -1, 1,
-// //     // Lower-right triangle
-// //     1, -1,
-// //     1, 1,
-// //     -1, 1
-// // ]), gl.STATIC_DRAW);
+    // // // progam2, read from intermediateTexture, write to screan
+    gl.useProgram(progarmInfo2.program)
+    twgl.setBuffersAndAttributes(gl,progarmInfo2,bufferInfo2)
+    twgl.resizeCanvasToDisplaySize(gl.canvas)
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
 
-// // gl.enableVertexAttribArray(texCoordAttributeLocation);
-// // var size = 2;          // 2 components per iteration
-// // var type = gl.FLOAT;   // the data is 32bit floats
-// // var normalize = false; // don't normalize the data
-// // var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-// // var offset = 0;        // start at the beginning of the buffer
-// // gl.vertexAttribPointer(
-// //     texCoordAttributeLocation, size, type, normalize, stride, offset)
+    gl.bindFramebuffer(gl.FRAMEBUFFER, sumBffer.framebuffer)
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, sumBffer.attachments[0], 0)
+    gl.bindTexture(gl.TEXTURE_2D, fbi.attachments[0])
+    gl.drawArrays(gl.TRIANGLES, 0, 6)
+    gl.bindFramebuffer(gl.FRAMEBUFFER,null)
 
-// // gl.useProgram(prog2);
-// // let mat3UniformLoc = gl.getUniformLocation(prog2, "u_matrix")
-// // gl.uniformMatrix3fv(mat3UniformLoc, false, conversionMat) 
+    // // // //   // // // //program 3
+    gl.useProgram(programInfo3.program)
+    gl.enable(gl.RASTERIZER_DISCARD);
+    twgl.setBuffersAndAttributes(gl, programInfo3, feedbackBuferInfo)
+    
+    gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, tf); 
+    gl.beginTransformFeedback(gl.POINTS)
 
-// // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);       
+    gl.bindTexture(gl.TEXTURE_2D, sumBffer.attachments[0])
+    gl.drawArrays(gl.POINTS, 0, 1)
 
-// // gl.drawArrays(gl.TRIANGLES, 0, 6);
-// console.timeEnd('time')
-// }
+    gl.endTransformFeedback();
+    gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, null);
+    
+    // // // turn on using fragment shaders again
+    gl.disable(gl.RASTERIZER_DISCARD);
 
-// main()
+
+    printResults(gl, feedbackBuferInfo.attribs.v_position.buffer, 'v_position')
+
+    function printResults(gl, buffer, label) {
+        const results = new Float32Array(2);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.getBufferSubData(
+            gl.ARRAY_BUFFER,
+            0,    // byte offset into GPU buffer,
+            results,
+        );
+
+        console.log(`${label}: ${results}`);
+    }
 
 
-// //helper functions
-// function createShader(gl, type, source) {
-//     var shader = gl.createShader(type);
-//     gl.shaderSource(shader, source);
-//     gl.compileShader(shader);
-//     var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-//     if (success) {
-//       return shader;
-//     }
-  
-//     console.log(gl.getShaderInfoLog(shader));  // eslint-disable-line
-//     gl.deleteShader(shader);
-//     return undefined;
-//   }
-  
-  
-//   function createProgram(gl, vertexShader, fragmentShader) {
-//     var program = gl.createProgram();
-//     gl.attachShader(program, vertexShader);
-//     gl.attachShader(program, fragmentShader);
-//     gl.linkProgram(program);
-//     var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-//     if (success) { 
-//       return program;
-//     }
-  
-//     console.log(gl.getProgramInfoLog(program));  // eslint-disable-line
-//     gl.deleteProgram(program);
-//     return undefined;
-//   }
-  
-//   function createEmptyTexture(gl:WebGL2RenderingContext,targetTextureWidth:number,targetTextureHeight:number,mipLevel:number) {
-//       var texture = gl.createTexture();
-//       gl.bindTexture(gl.TEXTURE_2D, texture);
-      
-//       const internalFormat = gl.RGBA;
-//       const border = 0;
-//       const format = gl.RGBA;
-//       const type = gl.UNSIGNED_BYTE;
-//       const data = null;
-//       gl.texImage2D(gl.TEXTURE_2D, mipLevel, internalFormat,
-//                     targetTextureWidth, targetTextureHeight, border,
-//                     format, type, data);
-//       // Set up texture so we can render any size image and so we are
-//       // working with pixels.
-//       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-//       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-//       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-//       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-   
-//       return texture;
-//   }
+
+}
+
+main();
--- src/app3.ts
+++ src/app3.ts
@@ -1,41 +1,43 @@
 import * as twgl from "twgl.js";
-const numberOfSites = 10000
+const numberOfSites = 100
+const canvas = document.querySelector("canvas"),
+gl = canvas.getContext("webgl2")
+////////////////for debugg////////////////////
+const pointVertexShader = `#version 300 es
 
-const vs2 = `#version 300 es
+in vec3 a_vertex;
+in vec2 a_position;
 
-in vec3 a_texCoord;
-out vec2 v_texCoord;
+void main() {  
+    gl_Position = vec4(a_vertex.xy + 2.0f * a_position.xy - 1.0f, a_vertex.z, 1);
+    gl_PointSize = 3.0;
+}
+`
 
-uniform mat3 u_matrix;
-
-void main() {
-    v_texCoord = vec3(u_matrix * vec3(a_texCoord.xy,1)).xy;
-    gl_Position = vec4(a_texCoord, 1.0);
-}`
-
-const fs2 = `#version 300 es
+const pointFragShader = `#version 300 es
 
 precision highp float;
 
-uniform sampler2D tex;
-in vec2 v_texCoord;
 out vec4 color;
 
 void main() {
-    color = texture(tex,v_texCoord);
+    color = vec4(0, 21, .2, 1);
 }
-`
+` 
+/////////////////////////////////////////////////////////
+
 const vs_srouce1 = `#version 300 es
 
 in vec3 a_vertex;
+//keep the positions in texture space -- 0 to 1//
 in vec2 a_position;
 out vec3 color;
 
 void main() {
+    gl_Position = vec4(a_vertex.xy + 2.0f * a_position.xy - 1.0f, a_vertex.z, 1);
     color.r = float(gl_InstanceID % 256) / 255.0;
     color.g = float((gl_InstanceID / 256) % 256) / 255.0;
     color.b = float((gl_InstanceID / 65536) % 256) / 255.0;   
-    gl_Position = vec4(a_vertex.xy + a_position.xy, a_vertex.z, 1);
 }
 `
 const fs_source1 = `#version 300 es
@@ -48,6 +50,16 @@ void main() {
   outColor = vec4(color.rgb, 1);
 }`
 
+/////////////////////////////////////////////////////////////
+
+const vs2 = `#version 300 es
+
+in vec2 a_texCoord;
+
+void main() {
+    gl_Position = vec4(a_texCoord, 1.0, 1.0);
+}`
+
 const sum_frag = `#version 300 es
 precision highp float;
 
@@ -57,9 +69,10 @@ uniform sampler2D voronoi;
 
 void main()
 {
+    color = vec4(0.0, 0.0, 0.0, 1.0);
     int my_index = int(gl_FragCoord.x);
     ivec2 tex_size = textureSize(voronoi, 0);
-    color = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    float count = 0.0f;
     for (int x=0; x < tex_size.x; x++)
     {
       ivec2 coord = ivec2(x, gl_FragCoord.y);
@@ -67,11 +80,10 @@ void main()
 
       // Unpack RGB value into a Voronoi cell index
       int i = int(255.0f * (t.r + (t.g * 256.0f) + (t.b * 65536.0f)));
-
       if (i == my_index)
       {
-          color.xy += vec2(coord);
-          color.z += 1.0f;
+          color.xy += vec2(coord).xy;
+          color.z += 1.0;
       }
     }
 
@@ -81,11 +93,9 @@ void main()
 
 const feedback_source = `#version 300 es
 
-in float index;
 out vec2 v_position;
 
 uniform sampler2D summed;
-uniform mat3 u_matrix;
 
 void main()
 {
@@ -94,21 +104,39 @@ void main()
     float count = 0.0f;
     for (int y=0; y < tex_size.y; ++y)
     {
-        ivec2 i = ivec2(index, y);
-        vec4 t = texelFetch(summed, i, 0);
+        vec4 t = texelFetch(summed, ivec2(gl_VertexID, y), 0); 
         v_position.xy += t.xy;
         count += t.z;
     }
-    vec3 p = vec3(v_position.xy,1.0);
-    v_position.xy = (u_matrix * p).xy;
-    // v_position.xy /= count;
+    v_position.xy /= count;
 }`
+
+//debug
+
+const debug_vertex = `#version 300 es
+
+in vec2 a_texCoord;
+
+void main() {
+    gl_Position = vec4(a_texCoord, 1.0, 1.0);
+}`
+
+const dubge_frag = `#version 300 es
+precision highp float;
+
+out vec4 color;
+
+void main() {
+  color = vec4(50.0, 168.0, 82.0, 1.0);
+}
+`
 
 //just enough to compile
 const feedback_fragment_shader = `#version 300 es
 precision highp float;
 
 void main() {
+  discard;
 }`
 
 
@@ -181,14 +209,15 @@ function createCone(edges:number) {
 
 
 
-const canvas = document.querySelector("canvas"),
-gl = canvas.getContext("webgl2")
 
+// const positionData = [.5,.5]
+const positionData = [...new Array(2 * numberOfSites)].map((_,i) => rand(0,1))
+console.log('original position: ', positionData)
 const arrays = {
   a_position: {
     numComponents: 2,
     divisor: 1,
-    data: [...new Array(2 * numberOfSites)].map((_,i) => rand(-1,1)),
+    data: positionData,
   },
   a_vertex: {
     numComponents: 3,
@@ -202,7 +231,7 @@ const arrays2 = {
   }
 }
 
-const range = [...new Array(numberOfSites).keys()]
+const range = [...new Float32Array(gl.canvas.width)]
 
 const array3 = {
   index:{
@@ -220,33 +249,27 @@ const arrays4 = {
   a_vertex: {
     numComponents: 3,
     data:createCone(36)
+}}
+
+const debugArrays = {
+  a_texCoord:{
+    numComponents: 2,
+    data: [-1, -1, 1, -1, -1, 1, 1, -1, 1, 1, -1, 1]
   }
 }
 
-const uniforms = {
-  u_matrix:[
-        .5, 0, 0,
-        0, .5, 0,
-        .5, .5, 1
-      ]
-}
-
-const uniforms2 = {
-  u_matrix:[
-    1, 0, 0,
-    0, 1, 0,
-    -1, -1, 1
-  ]
-}
 
 const progarmInfo1 = twgl.createProgramInfo(gl,[vs_srouce1,fs_source1])
 const progarmInfo2 = twgl.createProgramInfo(gl,[vs2,sum_frag])
 const programInfo3 = twgl.createProgramInfo(gl, [feedback_source,feedback_fragment_shader], {transformFeedbackVaryings:["v_position"]})
-const targetTexture = twgl.createTexture(gl,{target:gl.TEXTURE_2D,level:0,width:numberOfSites,height:600,min:gl.LINEAR,mag:gl.LINEAR})
+const drawPoints = twgl.createProgramInfo(gl,[pointVertexShader,pointFragShader])
+const debug = twgl.createProgramInfo(gl, [debug_vertex,dubge_frag])
+const targetTexture = twgl.createTexture(gl,{target:gl.TEXTURE_2D,level:0,width:numberOfSites,height:gl.canvas.height,min:gl.LINEAR,mag:gl.LINEAR,wrap:gl.CLAMP_TO_EDGE})
 const bufferInfo1 = twgl.createBufferInfoFromArrays(gl, arrays)
 const bufferInfo2 = twgl.createBufferInfoFromArrays(gl,arrays2)
 const bufferInfo3 = twgl.createBufferInfoFromArrays(gl, array3)
 const bufferInfo4 = twgl.createBufferInfoFromArrays(gl,arrays4)
+const debugBuffer = twgl.createBufferInfoFromArrays(gl,debugArrays)
 
 const fbo1 = gl.createFramebuffer()
 const fbo2 = gl.createFramebuffer()
@@ -262,85 +285,94 @@ gl.bindBuffer(gl.ARRAY_BUFFER, null)
 function main() {
   console.time('time')
   //program 1
-  twgl.resizeCanvasToDisplaySize(gl.canvas)
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-  gl.useProgram(progarmInfo1.program)
-  twgl.setBuffersAndAttributes(gl,progarmInfo1,bufferInfo1)  
-  //draw to farmebuffer backed by intermediate texture
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL);
-  gl.bindFramebuffer(gl.FRAMEBUFFER,fbo1)
-
-  createDepthTexture(gl,600,600,0)  
-  const intermediateTexture = createEmptyTexture(gl,600,600,0)
-  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, intermediateTexture, 0);
-  gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, arrays.a_vertex.data.length/3, arrays.a_position.data.length/2)
-
-  //program 2
-  gl.bindFramebuffer(gl.FRAMEBUFFER,null)
-  twgl.resizeCanvasToDisplaySize(gl.canvas)
-  gl.viewport(0, 0, numberOfSites, gl.canvas.height)
-  gl.useProgram(progarmInfo2.program)
-  twgl.setBuffersAndAttributes(gl,progarmInfo2,bufferInfo2)
-  twgl.setUniforms(progarmInfo2,uniforms)
-  gl.bindFramebuffer(gl.FRAMEBUFFER,null)
-  gl.bindFramebuffer(gl.FRAMEBUFFER,fbo2)
-  //draw from intermediate textrue to fbo2 backed by target textrue
-  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, 0)
-  gl.bindTexture(gl.TEXTURE_2D,intermediateTexture)
-  twgl.resizeCanvasToDisplaySize(gl.canvas)
-  gl.viewport(0, 0, numberOfSites, gl.canvas.height)
-  gl.drawArrays(gl.TRIANGLES, 0, 6)
-  gl.bindFramebuffer(gl.FRAMEBUFFER,null)
-
-  gl.bindFramebuffer(gl.FRAMEBUFFER,null)  
-  gl.bindTexture(gl.TEXTURE_2D,null)
-  gl.bindBuffer(gl.ARRAY_BUFFER,null)
-//   // // //program 3
-  gl.useProgram(programInfo3.program)
-  gl.enable(gl.RASTERIZER_DISCARD);
-  twgl.setBuffersAndAttributes(gl, programInfo3, bufferInfo3)
-  
-  gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, tf); 
-  gl.beginTransformFeedback(gl.POINTS)
-  // twgl.resizeCanvasToDisplaySize(gl.canvas)
-  twgl.setUniforms(programInfo3,uniforms2)
-  gl.viewport(0, 0, numberOfSites, gl.canvas.height)
-  gl.bindTexture(gl.TEXTURE_2D, targetTexture)
-  gl.drawArrays(gl.POINTS, 0, numberOfSites)
-
-  gl.endTransformFeedback();
-  gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, null);
  
-  // // turn on using fragment shaders again
-  gl.disable(gl.RASTERIZER_DISCARD);
-
-  gl.useProgram(progarmInfo1.program)
-
-  //bind to the transfrom feedback buffer
-  twgl.setBuffersAndAttributes(gl,progarmInfo1,bufferInfo4)  
-  // // //draw to farmebuffer backed by intermediate texture
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL);
+  gl.useProgram(debug.program)
+  twgl.setBuffersAndAttributes(gl,debug,debugBuffer)
   twgl.resizeCanvasToDisplaySize(gl.canvas)
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-  gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, arrays4.a_vertex.data.length/3, arrays4.a_position.data.length/2)
+  gl.drawArrays(gl.TRIANGLES, 0, 6)
+  // gl.useProgram(progarmInfo1.program)
+  // twgl.setBuffersAndAttributes(gl,progarmInfo1,bufferInfo1)  
+
+  //draw to farmebuffer backed by intermediate texture
+  // gl.enable(gl.DEPTH_TEST);
+  // gl.depthFunc(gl.LEQUAL);
+  // gl.bindFramebuffer(gl.FRAMEBUFFER,fbo1)
+
+  // createDepthTexture(gl,600,600,0)  
+  // const intermediateTexture = createEmptyTexture(gl,600,600,0)
+  // gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, intermediateTexture, 0);
+  // gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, arrays.a_vertex.data.length/3, arrays.a_position.data.length/2)
+  // gl.disable(gl.DEPTH_TEST)
+
+  // // //program 2
+  // gl.bindFramebuffer(gl.FRAMEBUFFER,null)
+  // twgl.resizeCanvasToDisplaySize(gl.canvas)
+  // gl.viewport(0, 0, numberOfSites, gl.canvas.height)
+  // gl.useProgram(progarmInfo2.program)
+  // twgl.setBuffersAndAttributes(gl,progarmInfo2,bufferInfo2)
+  // gl.bindFramebuffer(gl.FRAMEBUFFER,fbo2)
+  // // draw from intermediate textrue to fbo2 backed by target textrue
+  // gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, targetTexture, 0)
+  // gl.bindTexture(gl.TEXTURE_2D,intermediateTexture)
+  // gl.drawArrays(gl.TRIANGLES, 0, 6)
+  // gl.bindFramebuffer(gl.FRAMEBUFFER,null) 
+  // gl.bindTexture(gl.TEXTURE_2D,null)
+  // gl.bindBuffer(gl.ARRAY_BUFFER,null)
+
+  // // // //program 3
+  // gl.useProgram(programInfo3.program)
+  // gl.enable(gl.RASTERIZER_DISCARD);
+  // twgl.setBuffersAndAttributes(gl, programInfo3, bufferInfo3)
+  
+  // gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, tf); 
+  // gl.beginTransformFeedback(gl.POINTS)
+  // twgl.resizeCanvasToDisplaySize(gl.canvas)
+  // gl.viewport(0, 0, numberOfSites, gl.canvas.height)
+  // gl.bindTexture(gl.TEXTURE_2D, targetTexture)
+  // gl.drawArrays(gl.POINTS, 0, numberOfSites)
+
+  // gl.endTransformFeedback();
+  // gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, null);
+ 
+  // // // turn on using fragment shaders again
+  // gl.disable(gl.RASTERIZER_DISCARD);
+
+  // //   //////////debug code///////
+  // gl.bindBuffer(gl.ARRAY_BUFFER,null)
+  // twgl.resizeCanvasToDisplaySize(gl.canvas)
+  // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  // gl.useProgram(progarmInfo1.program)
+
+
+  // twgl.setBuffersAndAttributes(gl,progarmInfo1,bufferInfo1)
+  // gl.enable(gl.DEPTH_TEST);
+  // gl.depthFunc(gl.LEQUAL);
+  // gl.drawArraysInstanced(gl.TRIANGLE_FAN, 0, arrays.a_vertex.data.length/3, arrays.a_position.data.length/2)
+
+  // gl.useProgram(drawPoints.program)
+  // twgl.setBuffersAndAttributes(gl,drawPoints,bufferInfo4)  
+
+  // twgl.resizeCanvasToDisplaySize(gl.canvas)
+  // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  // gl.drawArraysInstanced(gl.POINTS, 0, arrays4.a_vertex.data.length/3, arrays4.a_position.data.length/2)
+
 
   console.timeEnd('time')
 
-  // printResults(gl, bufferInfo4.attribs.a_position.buffer, 'v_position')
+  printResults(gl, bufferInfo4.attribs.a_position.buffer, 'v_position')
 
-  // function printResults(gl, buffer, label) {
-  //   const results = new Float32Array(numberOfSites * 2);
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  //   gl.getBufferSubData(
-  //       gl.ARRAY_BUFFER,
-  //       0,    // byte offset into GPU buffer,
-  //       results,
-  //   );
+  function printResults(gl, buffer, label) {
+    const results = new Float32Array(numberOfSites * 2);
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.getBufferSubData(
+        gl.ARRAY_BUFFER,
+        0,    // byte offset into GPU buffer,
+        results,
+    );
 
-  //   console.log(`${label}: ${results}`);
-  // }
+    console.log(`${label}: ${results}`);
+  }
 }
 
 main();
